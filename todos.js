beca0ccef163c89331e3b668d7d9af4bb45da1e0
msolorio <msolorio.github@gmail.com>
2021-12-27 18:01:45 -0800

    Adds 2 todos

--- todos.js
+++ todos.js
@@ -11,5 +11,13 @@ module.exports = [
   {
     id: 3,
     task: "Wash the dishes"
+  },
+  {
+    id: 4,
+    task: "Make dinner"
+  },
+  {
+    id: 5,
+    task: "Build automation for circle ci setup"
   }
 ];
